4300cd45f2779e618030caa74b7beaf4ae7ab275
huongtmn <ntmaihuong1997@gmail.com>
2022-11-16 13:26:53 +0700

    processing

---
+++ services/index.js
@@ -0,0 +1,48 @@
+// Data hardcode cần thay thế bằng API
+let prod = new Products (
+    "iphoneX",
+    1000,
+    "screen 68",
+    "2 camera 12 MP",
+    "7 MP",
+    "https://cdn.tgdd.vn/Products/Images/42/114115/iphone-x-64gb-hh-600x600.jpg",
+    "Thiết kế mang tính đột phá",
+    "iphone",
+);
+
+let prodArr = [prod,prod,prod,prod,prod,prod,prod,prod]
+
+const domId = (id) => document.getElementById (id);
+
+const renderProductList = (prodArr) => {
+    let content = "";
+    
+    for(let i in prodArr) {
+        content +=`
+        <div class="col-3 product-item">
+                    <div class="product">
+                        <div class="product_image center">
+                            <img src="${prodArr[i].img}" alt="">
+                        </div>
+                        <div class="favorite"></div>
+                        <div class="product_info">
+                            <h6 class="product_name">${prodArr[i].name}</h6>
+                            <div class="product_price">${prodArr[i].price}</div>
+                            <div class="product_screen">${prodArr[i].screen}</div>
+                            <div class="product_backCamera">${prodArr[i].backCamera}</div>
+                            <div class="product_frontCamera">${prodArr[i].frontCamera}</div>
+                            <div class="product_desc">${prodArr[i].desc}</div>
+                            <div class="product_type">${prodArr[i].type}</div>
+                        </div>
+                    </div>
+                    <div class="red_button add_to_cart_button"><a href="#">add to cart</a></div>
+                </div>
+        `
+    }
+    
+    domId("product-list").innerHTML = content;
+}
+
+window.onload = () => {
+    renderProductList(prodArr);
+}
---
+++ services/products.js
@@ -0,0 +1,12 @@
+// Khai báo lớp đối tượng
+function Products(name, price, screen, blackCamera, frontCamera, img, desc, type) {
+    this.name = name,
+    this.price = price,
+    this.screen = screen;
+    this.blackCamera = blackCamera;
+    this.frontCamera = frontCamera;
+    this.img = img;
+    this.desc = desc;
+    this.type = type;
+}
+
