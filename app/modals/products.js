bb5814af23c5d27eac9a72b7f2f1b9103daeec59
huongtmn <huongnguyen.aud@gmail.com>
2022-11-21 13:49:10 +0700

    done userUI

--- app/modals/products.js
+++ app/modals/products.js
@@ -1,5 +1,6 @@
 // Khai báo lớp đối tượng
-function Products(name, price, screen, backCamera, frontCamera, img, desc, type) {
+function Products(id, name, price, screen, backCamera, frontCamera, img, desc, type) {
+    this.id = id,
     this.name = name,
     this.price = price,
     this.screen = screen;
