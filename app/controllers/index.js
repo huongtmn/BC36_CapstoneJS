85a98a680a228c993ae4fd2feaf0250ce31eb46e
Minh Nguyen <nttminh234@gmail.com>
2022-11-22 22:32:36 +0700

    filter product

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,48 +1,75 @@
-// Data hardcode cần thay thế bằng API
-let prod = new Products (
-    "iphoneX",
-    "$1000",
-    "screen 68",
-    "2 camera 12 MP",
-    "7 MP",
-    "https://cdn.tgdd.vn/Products/Images/42/114115/iphone-x-64gb-hh-600x600.jpg",
-    "Thiết kế mang tính đột phá",
-    "iphone",
-);
+let productsService = new ProductsService();
 
-// Test data
-let prodArr = [prod,prod,prod,prod,prod,prod,prod,prod]
+const domId = (id) => document.getElementById(id);
 
-const domId = (id) => document.getElementById (id);
+let getProductList = () => {
+  productsService.getList().then((response) => {
+    renderProductList(response.data);
+  });
+};
 
-const renderProductList = (prodArr) => {
-    let content = "";
-    
-    for(let i in prodArr) {
-        content +=`
+let renderProductList = (data) => {
+  let content = "";
+  for (let i in data) {
+    content += `
         <div class="col-3 product-item">
-                    <div class="product">
+                     <div class="product">
                         <div class="product_image center">
-                            <img src="${prodArr[i].img}" alt="">
+                            <img src="${data[i].img}" alt="">
                         </div>
                         <div class="product_info">
-                            <h6 class="product_name">${prodArr[i].name}</h6>
-                            <div class="product_price">${prodArr[i].price}</div>
-                            <div class="product_screen">${prodArr[i].screen}</div>
-                            <div class="product_backCamera">${prodArr[i].backCamera}</div>
-                            <div class="product_frontCamera">${prodArr[i].frontCamera}</div>
-                            <div class="product_desc">${prodArr[i].desc}</div>
-                            <div class="product_type" style="display: none;">${prodArr[i].type}</div>
+                            <h6 class="product_name">${data[i].name}</h6>
+                             <div class="product_price">${data[i].price}</div>
+                             <div class="product_screen">${data[i].screen}</div>
+                             <div class="product_backCamera">${data[i].backCamera}</div>
+                             <div class="product_frontCamera">${data[i].frontCamera}</div>
+                             <div class="product_desc">${data[i].desc}</div>
+                             <div class="product_type" style="display: none;">${data[i].type}</div>
                         </div>
-                    </div>
-                    <div id="btnAđ" class="red_button add_to_cart_button"><a href="#">add to cart</a></div>
-                </div>
-        `
-    }
-    
-    domId("product-list").innerHTML = content;
+                     </div>
+                     <div id="btnAđ" class="red_button add_to_cart_button"><a href="#">add to cart</a></div>
+                 </div>
+        `;
+  }
+  domId("product-list").innerHTML = content;
+};
+
+
+
+// FILTER SẢN PHẨM
+
+let arrayPhoneList = [];
+for (let i in getProductList) {
+  if (getProductList.hasOwnProperty(i)) {
+    arrayPhoneList.push(i);
+  }
 }
 
+filterPhoneList = (type) => {
+  const data = this.arrayPhoneList.filter((element) => {
+    if (element.type === type) {
+      return true;
+    }
+
+    return false;
+  });
+
+  return data;
+};
+
+domId("phones").onchange = (event) => {
+  const value = event.target.value;
+
+  const data = filterPhoneList(value);
+
+  renderProductList(data);
+};
+
+
 window.onload = () => {
-    renderProductList(prodArr);
-}
+  getProductList();
+};
+
+
+
+
---
+++ app/service/products.js
@@ -0,0 +1,9 @@
+function ProductsService(id) {
+    this.getList = function () {
+      return axios({
+        url: "https://63661fbc046eddf1baf95efc.mockapi.io/Products",
+        method: "GET",
+      });
+    };
+  }
+  
